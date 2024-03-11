c25cc5c97a7d2e0c900a984157ad98503e21b67f
Nikita <nick.parser@gmail.com>
2024-03-11 19:25:07 +0100

    logger & shift fixes

--- editor/src/command/control.rs
+++ editor/src/command/control.rs
@@ -4,12 +4,17 @@ use crate::workspace::Workspace;
 pub(super) fn open_close_logger(workspace: &mut Workspace) {
     if workspace.logger_active() {
         // at this point we do not care about the last active buffer
-        let id = workspace.buffers.keys().last().unwrap();
+        let logger_id = workspace.logger_id();
+        let id = workspace
+            .buffers
+            .keys()
+            .find(|id| **id != logger_id)
+            .unwrap();
         workspace.set_current_id(*id);
 
         return;
     }
 
     let id = workspace.logger_id();
-    workspace.set_current_id(id);
+    workspace.set_current_id(id)
 }
