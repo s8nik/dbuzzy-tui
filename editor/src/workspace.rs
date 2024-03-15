bc9635b7c102fe0434a30930639e7e56bdebc119
Nikita <nick.parser@gmail.com>
2024-03-15 19:27:38 +0100

    workspace is removed

--- editor/src/workspace.rs
+++ editor/src/workspace.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::HashMap,
     fs::File,
     io::BufReader,
     num::NonZeroUsize,
@@ -15,6 +14,8 @@ use crate::{buffer::Buffer, history::History};
 pub struct DocumentId(NonZeroUsize);
 
 impl DocumentId {
+    pub const MAX: Self = Self(unsafe { NonZeroUsize::new_unchecked(usize::MAX) });
+
     pub fn next() -> Self {
         pub static IDS: AtomicUsize = AtomicUsize::new(1);
 
@@ -41,23 +42,12 @@ pub struct FileMeta {
 pub struct Document {
     id: DocumentId,
     meta: FileMeta,
-    buffer: Buffer,
-    history: History,
+
+    pub(super) buffer: Buffer,
+    pub(super) history: History,
 }
 
 impl Document {
-    pub fn logger() -> Self {
-        Self {
-            id: DocumentId::next(),
-            buffer: Buffer::logger(),
-            history: History::default(),
-            meta: FileMeta {
-                path: None,
-                readonly: true,
-            },
-        }
-    }
-
     pub fn from_path(path: impl AsRef<Path>) -> anyhow::Result<Self> {
         let path = path.as_ref();
         let mut document = Self::default();
@@ -86,57 +76,5 @@ impl Document {
 
     pub const fn id(&self) -> DocumentId {
         self.id
-    }
-
-    pub const fn buf(&self) -> &Buffer {
-        &self.buffer
-    }
-
-    pub fn buf_mut(&mut self) -> &mut Buffer {
-        &mut self.buffer
-    }
-}
-
-#[derive(Default)]
-pub struct Workspace {
-    documents: HashMap<DocumentId, Document>,
-    current: DocumentId,
-    logger: DocumentId,
-}
-
-impl Workspace {
-    pub fn current(&self) -> &Document {
-        self.documents.get(&self.current).expect("current buff")
-    }
-
-    pub fn current_mut(&mut self) -> &mut Document {
-        self.documents
-            .get_mut(&self.current)
-            .expect("current mut buff")
-    }
-
-    pub fn logger(&mut self) -> Option<&mut Document> {
-        self.documents.get_mut(&self.logger)
-    }
-
-    pub fn add_document(&mut self, document: Document, set_current: bool) {
-        let id = document.id();
-        self.documents.insert(id, document);
-
-        if set_current {
-            self.current = id;
-        }
-    }
-
-    pub fn init_logger(&self) {
-        let document = Document::logger();
-        let id = document.id();
-
-        self.documents.insert(id, document);
-        self.logger = id;
-    }
-
-    pub fn logger_active(&self) -> bool {
-        self.current == self.logger
     }
 }
