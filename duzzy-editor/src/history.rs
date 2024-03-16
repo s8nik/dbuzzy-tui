96c36e576d0a08ac10df1e249f38897f93b56905
Nikita <nick.parser@gmail.com>
2024-03-16 21:05:45 +0100

    commit add/push

--- duzzy-editor/src/history.rs
+++ duzzy-editor/src/history.rs
@@ -1,52 +1,68 @@
-use crate::buffer::{CursorMode, Position};
+use std::collections::VecDeque;
 
-// 1. N
-// history -> add('N', &buffer)
-//  -
+use crate::buffer::Position;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Change {
     Insert(String),
     Delete(String),
 }
 
-pub struct CommitData {
-    pub mode: CursorMode,
-    pub position: Position,
-}
-
 pub struct Commit {
-    before: CommitData,
-    after: CommitData,
+    before: Position,
+    after: Position,
     change: Change,
-}
-
-pub struct Transaction {
-    current: Commit,
-    inverted: Commit,
-}
-
-impl Transaction {
-    pub fn apply() {}
 }
 
 #[derive(Default)]
 pub struct History {
-    index: usize,
+    head: usize,
     max_items: usize,
-    commits: Vec<Transaction>,
+    commits: VecDeque<Commit>,
 }
 
 impl History {
     pub fn new(max_items: usize) -> Self {
         Self {
-            index: 0,
+            head: 0,
             max_items,
-            commits: Vec::with_capacity(max_items),
+            commits: VecDeque::with_capacity(max_items),
         }
     }
 
-    pub fn add() {}
+    pub fn add(&mut self, commit: Commit) {
+        if self.max_items == 0 {
+            return;
+        }
+
+        match self.commits.back_mut() {
+            Some(last) => match (&mut last.change, &commit.change) {
+                (Change::Insert(prev), Change::Insert(change))
+                | (Change::Delete(prev), Change::Delete(change)) => prev.push_str(change),
+                _ => self.push(commit),
+            },
+            _ => self.push(commit),
+        }
+    }
+
+    fn push(&mut self, commit: Commit) {
+        if self.commits.len() == self.max_items {
+            self.commits.pop_front();
+            self.head = self.head.saturating_sub(1);
+        }
+
+        self.commits.push_back(commit);
+        self.head += 1;
+    }
 
     pub fn undo(text: &mut ropey::Rope) {}
     pub fn redo(text: &mut ropey::Rope) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_history() {}
 }
