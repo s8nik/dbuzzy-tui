c96422ec691f4dbfd2c9b2871060e7ff731060ad
Nikita <nick.parser@gmail.com>
2024-03-24 21:33:59 +0100

    unused fmt display for cursor mode

--- duzzy-editor/src/buffer.rs
+++ duzzy-editor/src/buffer.rs
@@ -112,13 +112,3 @@ pub enum CursorMode {
     Normal,
     Visual,
 }
-
-impl std::fmt::Display for CursorMode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Insert => write!(f, "insert"),
-            Self::Normal => write!(f, "normal"),
-            Self::Visual => write!(f, "visual"),
-        }
-    }
-}
