a770252dfe22a0563f2e33efce71f4e80d93ec5a
Nikita <nick.parser@gmail.com>
2024-04-08 17:38:06 +0200

    delete selection

--- duzzy-editor/src/command/adjustment.rs
+++ duzzy-editor/src/command/adjustment.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use crate::{
     editor::Workspace,
     transaction::{Transaction, TransactionResult},
@@ -42,11 +44,30 @@ pub(super) fn new_line(ws: &mut Workspace) {
     });
 }
 
-pub(super) fn delete_char_inplace(ws: &mut Workspace) {
+pub(super) fn delete(ws: &mut Workspace) {
     let doc = ws.curr_mut();
 
     doc.with_transaction(|tx, buf| {
         let pos = buf.byte_pos();
+
+        if let Some(selection) = buf.selection() {
+            let (start, end) = selection.range();
+            let len_chars = buf.len_chars();
+
+            let slice = buf.text().slice(start..(end + 1).min(len_chars));
+            let slice = match slice.as_str() {
+                Some(s) => Cow::from(s),
+                None => Cow::from(slice.to_string()),
+            };
+
+            tx.delete_str(start, &slice);
+            if let Some(pos) = tx.apply(buf.text_mut()) {
+                buf.set_pos(buf.curs_pos(pos));
+            }
+
+            super::switch_mode::visual_to_normal_impl(buf);
+            return TransactionResult::Commit;
+        }
 
         if pos < buf.len_chars() {
             let ch = buf.char(pos);
@@ -61,7 +82,7 @@ pub(super) fn delete_char_inplace(ws: &mut Workspace) {
     });
 }
 
-pub(super) fn delete_char(ws: &mut Workspace) {
+pub(super) fn delete_backspace(ws: &mut Workspace) {
     let doc = ws.curr_mut();
 
     doc.with_transaction(|delete_tx, buf| {
@@ -106,8 +127,8 @@ mod tests {
         assert_eq!((0, 4), buf.pos());
         assert_eq!(&buf.text().to_string(), "test");
 
-        delete_char(&mut ws);
-        delete_char(&mut ws);
+        delete_backspace(&mut ws);
+        delete_backspace(&mut ws);
 
         let buf = ws.curr().buf();
         assert_eq!((0, 2), buf.pos());
@@ -125,12 +146,29 @@ mod tests {
 
         ws.curr_mut().commit();
 
-        delete_char_inplace(&mut ws);
-        delete_char_inplace(&mut ws);
-        delete_char_inplace(&mut ws);
+        delete(&mut ws);
+        delete(&mut ws);
+        delete(&mut ws);
 
         let buf = ws.curr().buf();
         assert_eq!((4, 0), buf.pos());
         assert_eq!(&buf.text().to_string(), "\n\n\n\n");
+    }
+
+    #[test]
+    fn test_delete_selection() {
+        let mut ws = Workspace::default();
+        ws.add_doc(Document::default());
+
+        let text = ropey::Rope::from_str("test \ntest");
+        let doc = ws.curr_mut();
+        let buf = doc.buf_mut();
+        buf.set_text(text);
+        buf.new_selection(buf.len_chars());
+        buf.update_selection(4);
+
+        delete(&mut ws);
+
+        assert_eq!(&ws.curr().buf().text().to_string(), "test");
     }
 }
