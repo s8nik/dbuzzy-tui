2ace6a44dd30233b5cbfa28e343ca7e0dc070aa3
Nikita <nick.parser@gmail.com>
2024-05-12 00:03:50 +0200

    search iter impl

--- duzzy-editor/src/search.rs
+++ duzzy-editor/src/search.rs
@@ -3,26 +3,27 @@ use ropey::RopeSlice;
 pub type MatchRange = (usize, usize);
 
 struct SearchIter<'a> {
-    cur_index: usize,
+    cur_ofs: usize,
     pattern: &'a str,
     pattern_len: usize,
-    chars: ropey::iter::Chars<'a>,
-    matches: Vec<std::str::Chars<'a>>,
+    char_iter: ropey::iter::Chars<'a>,
+    possible_matches: Vec<std::str::Chars<'a>>,
 }
 
 impl<'a> SearchIter<'a> {
     fn from_rope_slice<'b>(slice: &'b RopeSlice, search_pattern: &'b str) -> SearchIter<'b> {
+        assert!(slice.len_chars() != 0, "Text couldn't be empty.");
         assert!(
             !search_pattern.is_empty(),
             "Can't search using an empty search pattern."
         );
 
         SearchIter {
-            cur_index: 0,
-            chars: slice.chars(),
+            cur_ofs: 0,
+            char_iter: slice.chars(),
             pattern: search_pattern,
             pattern_len: search_pattern.chars().count(),
-            matches: Vec::new(),
+            possible_matches: Vec::new(),
         }
     }
 }
@@ -31,7 +32,28 @@ impl Iterator for SearchIter<'_> {
     type Item = MatchRange;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        while let Some(next_char) = self.char_iter.by_ref().next() {
+            self.cur_ofs += 1;
+            self.possible_matches.push(self.pattern.chars());
+
+            let mut i = 0;
+            while i < self.possible_matches.len() {
+                let pattern_char = self.possible_matches[i].next().unwrap();
+                if next_char == pattern_char {
+                    if self.possible_matches[i].clone().next().is_none() {
+                        let char_match_range = (self.cur_ofs - self.pattern_len, self.cur_ofs);
+                        self.possible_matches.clear();
+                        return Some(char_match_range);
+                    } else {
+                        i += 1;
+                    }
+                } else {
+                    let _ = self.possible_matches.swap_remove(i);
+                }
+            }
+        }
+
+        None
     }
 }
 
