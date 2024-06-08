e9c8de96e04500d2a3e29b46b9ee2f0eb01d6c09
Nikita <nick.parser@gmail.com>
2024-06-08 22:49:36 +0200

    on_key is started

--- duzzy-editor/src/command/search.rs
+++ duzzy-editor/src/command/search.rs
@@ -1,11 +1,30 @@
-use crate::{buffer::Mode, editor::Workspace, event::Input, renderer::EventOutcome};
+use crate::{
+    editor::Workspace,
+    event::{Event, Input},
+    renderer::EventOutcome,
+};
 
 pub fn on_key(ws: &mut Workspace, input: Input) -> EventOutcome {
+    match input {
+        Input {
+            event: Event::Esc, ..
+        } => super::normal_mode(ws),
+        Input {
+            event: Event::Enter,
+            ..
+        } => apply_search(ws),
+        Input {
+            event: Event::Char(ch),
+            ..
+        } => todo!(),
+        _ => todo!(),
+    };
+
     todo!()
 }
 
-pub(super) fn search_mode(ws: &mut Workspace) {
-    ws.cur_mut().buf_mut().set_mode(Mode::Search)
+fn apply_search(ws: &mut Workspace) {
+    todo!()
 }
 
 pub(super) fn search_next(ws: &mut Workspace) {
