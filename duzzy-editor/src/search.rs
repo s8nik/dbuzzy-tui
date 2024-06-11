a48a171d46c74a9422bef8592bbe2fcf496d8fe1
Nikita <nick.parser@gmail.com>
2024-06-11 19:30:30 +0200

    iterator is removed

--- duzzy-editor/src/search.rs
+++ duzzy-editor/src/search.rs
@@ -1,122 +1,78 @@
 use std::collections::HashMap;
 
-use ropey::RopeSlice;
+use ropey::Rope;
 
 use crate::SmartString;
 
 pub type MatchRange = (usize, usize);
 
-pub struct SearchRegistry<'a> {
-    done: bool,
-    index: usize,
-    matches: Vec<MatchRange>,
-    iter: SearchIter<'a>,
-}
-
-impl<'a> SearchRegistry<'a> {
-    pub fn new(text: RopeSlice<'a>, pattern: SmartString) -> Self {
-        Self {
-            index: 0,
-            done: false,
-            matches: vec![],
-            iter: SearchIter::from_rope_slice(text, pattern),
-        }
-    }
-
-    pub fn pattern(&self) -> &str {
-        self.iter.pattern.as_str()
-    }
-
-    pub fn next(&mut self) -> Option<MatchRange> {
-        if self.done {
-            if self.index == self.matches.len() - 1 {
-                self.index = 0;
-            }
-
-            return self.matches.get(self.index).cloned();
-        }
-
-        let Some(range) = self.iter.next() else {
-            self.done = true;
-            self.index = 0;
-            return self.matches.get(self.index).cloned();
-        };
-
-        self.index += 1;
-        self.matches.push(range);
-
-        Some(range)
-    }
-
-    pub fn prev(&mut self) -> Option<MatchRange> {
-        self.index = if self.index == 0 {
-            self.matches.len() - 1
-        } else {
-            self.index - 1
-        };
-
-        self.matches.get(self.index).cloned()
-    }
-}
-
-#[derive(Debug)]
-struct SearchIter<'a> {
-    text: RopeSlice<'a>,
-    offset: usize,
-    pattern_len: usize,
-    pattern: SmartString,
+#[derive(Default)]
+pub struct SearchRegistry {
+    buffer: SmartString,
+    pattern: Option<SmartString>,
     bad_match_table: HashMap<char, usize>,
 }
 
-impl<'a> SearchIter<'a> {
-    fn from_rope_slice(slice: RopeSlice<'a>, search_pattern: SmartString) -> SearchIter<'a> {
-        assert!(slice.len_chars() != 0, "The text cannot be empty.");
+impl SearchRegistry {
+    pub fn insert_char(&mut self, ch: char) {
+        self.buffer.push(ch);
+    }
+
+    pub fn remove_char(&mut self, ch: char) {
+        self.buffer.pop();
+    }
+
+    #[cfg(test)]
+    pub fn set_pattern(&mut self, pattern: SmartString) {
+        self.buffer = pattern;
+        self.apply();
+    }
+
+    pub fn apply(&mut self) {
+        if self.buffer.is_empty() {
+            return;
+        }
+
+        self.pattern = Some(self.buffer.to_owned());
+        self.bad_match_table = HashMap::new();
+
+        let pattern_len = self.buffer.len();
+        for (i, c) in self.buffer.chars().enumerate() {
+            let shift = 1.max(pattern_len - i - 1);
+            self.bad_match_table.insert(c, shift);
+        }
+
+        self.buffer.clear();
+    }
+
+    pub fn cancel(&mut self) {
+        self.buffer.clear();
+    }
+
+    pub fn search(&self, text: &Rope, start_pos: usize) -> Option<MatchRange> {
+        assert!(text.len_chars() != 0, "The text cannot be empty.");
+
+        let pattern = self.pattern.as_ref()?;
+        let pattern_len = pattern.len();
+
         assert!(
-            !search_pattern.is_empty(),
+            !pattern.is_empty(),
             "Can't search using an empty search pattern."
         );
 
-        let pattern_len = search_pattern.len();
-        let bad_match_table = Self::bad_match_table(&search_pattern, pattern_len);
+        let mut index = start_pos;
 
-        SearchIter {
-            offset: 0,
-            text: slice,
-            pattern: search_pattern,
-            pattern_len,
-            bad_match_table,
-        }
-    }
-
-    fn bad_match_table(search_pattern: &str, pattern_len: usize) -> HashMap<char, usize> {
-        let mut table = HashMap::new();
-
-        for (i, c) in search_pattern.chars().enumerate() {
-            let shift = 1.max(pattern_len - i - 1);
-            table.insert(c, shift);
-        }
-
-        table
-    }
-}
-
-impl Iterator for SearchIter<'_> {
-    type Item = MatchRange;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut index = self.offset;
-
-        while index <= self.text.len_chars() - self.pattern_len {
+        while index <= text.len_chars() - pattern_len {
             let mut skips = 0;
 
-            for i in (0..self.pattern_len).rev() {
-                let ch_text = self.text.char(index + i);
-                let ch_pattern = self.pattern.chars().nth(i)?;
+            for i in (0..pattern_len).rev() {
+                let ch_text = text.char(index + i);
+                let ch_pattern = pattern.chars().nth(i)?;
 
                 if ch_pattern != ch_text {
                     match self.bad_match_table.get(&ch_text) {
                         Some(s) => skips = *s,
-                        None => skips = self.pattern_len,
+                        None => skips = pattern_len,
                     };
 
                     break;
@@ -124,8 +80,7 @@ impl Iterator for SearchIter<'_> {
             }
 
             if skips == 0 {
-                let offset = index + self.pattern_len;
-                self.offset = offset;
+                let offset = index + pattern_len;
                 return Some((index, offset));
             }
 
@@ -140,28 +95,32 @@ impl Iterator for SearchIter<'_> {
 mod tests {
     use ropey::Rope;
 
-    use super::SearchIter;
+    use super::SearchRegistry;
 
     #[test]
     fn test_search_single_line() {
         let text = Rope::from_str("lotestlol");
-        let mut iter = SearchIter::from_rope_slice(text.slice(..), "lo".into());
+        let mut registry = SearchRegistry::default();
 
-        assert_eq!(iter.next(), Some((0, 2)));
-        assert_eq!(iter.next(), Some((6, 8)));
-        assert_eq!(iter.next(), None);
+        registry.set_pattern("lo".into());
 
-        let mut iter = SearchIter::from_rope_slice(text.slice(..), "test".into());
-        assert_eq!(iter.next(), Some((2, 6)));
-        assert_eq!(iter.next(), None);
+        assert_eq!(registry.search(&text, 0), Some((0, 2)));
+        assert_eq!(registry.search(&text, 3), Some((6, 8)));
+        assert_eq!(registry.search(&text, 7), None);
+
+        registry.set_pattern("test".into());
+
+        assert_eq!(registry.search(&text, 0), Some((2, 6)));
+        assert_eq!(registry.search(&text, 4), None);
     }
 
     #[test]
     fn test_search_multiple_line() {
         let text = Rope::from_str("foo line1\nbar line2");
-        let mut iter = SearchIter::from_rope_slice(text.slice(..), "line".into());
+        let mut registry = SearchRegistry::default();
+        registry.set_pattern("line".into());
 
-        assert_eq!(iter.next(), Some((4, 8)));
-        assert_eq!(iter.next(), Some((14, 18)));
+        assert_eq!(registry.search(&text, 0), Some((4, 8)));
+        assert_eq!(registry.search(&text, 6), Some((14, 18)));
     }
 }
