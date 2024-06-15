2afe81e76e5f386a393976a22f0ab81fa26ab198
Nikita <nick.parser@gmail.com>
2024-06-15 23:39:00 +0200

    read conf from file

--- duzzy-db/src/config.rs
+++ duzzy-db/src/config.rs
@@ -1,3 +1,9 @@
+use std::{io::Read, path::PathBuf};
+
+use anyhow::Context;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 pub struct ConnConfig {
     name: String,
     host: String,
@@ -5,6 +11,38 @@ pub struct ConnConfig {
     db: Option<String>,
     user: String,
     password: Option<String>,
+}
+
+impl ConnConfig {
+    // @todo: should be moved as it is general purpose
+    pub fn from_file() -> anyhow::Result<Self> {
+        // @note: linux & macos support only for now
+        let mut config_path: PathBuf = std::env::var("$HOME")
+            .with_context(|| "HOME env var")?
+            .into();
+
+        config_path.push(".config/dbuzzy");
+
+        if !config_path.try_exists()? {
+            std::fs::create_dir_all(&config_path)?;
+        }
+
+        config_path.push(std::env!("CARGO_PKG_NAME"));
+        config_path.set_extension("toml");
+
+        if let Ok(file) = std::fs::File::open(config_path) {
+            let mut reader = std::io::BufReader::new(file);
+            let mut raw_config = String::new();
+            reader.read_to_string(&mut raw_config)?;
+
+            match toml::from_str(&raw_config) {
+                Ok(c) => return Ok(c),
+                Err(e) => anyhow::bail!("fail to parse config file: {e}"),
+            }
+        }
+
+        Ok(ConnConfigBuilder::new().build())
+    }
 }
 
 #[derive(Default)]
