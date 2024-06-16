62975c8d052946adaff5a3f1660cc578492cdbca
Nikita <nick.parser@gmail.com>
2024-06-16 15:18:21 +0200

    custom error type

--- duzzy-db/src/lib.rs
+++ duzzy-db/src/lib.rs
@@ -6,3 +6,13 @@
 )]
 pub mod config;
 pub mod connection;
+
+pub type DbResult<T> = anyhow::Result<T, Error>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Pool error: {0}")]
+    PoolError(#[from] deadpool_postgres::PoolError),
+    #[error("PostgreSQL error: {0}")]
+    PgError(#[from] tokio_postgres::Error),
+}
