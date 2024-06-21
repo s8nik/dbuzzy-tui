abca32ae99c68da8704bcc2a3eb6bb823a58d20d
Nikita <nick.parser@gmail.com>
2024-06-21 20:32:41 +0200

    conn widget small fixes & refactoring

--- dbuzzy/src/widgets/connections.rs
+++ dbuzzy/src/widgets/connections.rs
@@ -1,4 +1,5 @@
-use duzzy_lib::{colors, DrawableStateful};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
+use duzzy_lib::{colors, DrawableStateful, EventOutcome, OnEvent};
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Layout, Rect},
@@ -16,9 +17,15 @@ pub struct ConnWidget {
 
 impl ConnWidget {
     pub fn new(conns: Vec<ConnCfg>) -> Self {
+        let mut state = ListState::default();
+
+        if !conns.is_empty() {
+            state.select(Some(0));
+        }
+
         Self {
             items: conns,
-            state: ListState::default(),
+            state,
         }
     }
 
@@ -26,7 +33,7 @@ impl ConnWidget {
         let i = self
             .state
             .selected()
-            .map(|i| if i > self.items.len() - 1 { 0 } else { i + 1 });
+            .map(|i| if i >= self.items.len() - 1 { 0 } else { i + 1 });
 
         self.state.select(i);
     }
@@ -47,37 +54,59 @@ impl ConnWidget {
 
 impl DrawableStateful for ConnWidget {
     fn draw(&mut self, area: Rect, buf: &mut Buffer) {
-        let vertical = Layout::vertical([
-            Constraint::Length(2),
-            Constraint::Min(0),
-            Constraint::Length(2),
-        ]);
+        let vertical = Layout::vertical([Constraint::Min(0), Constraint::Length(2)]);
 
-        let [header_area, rest_area, footer_area] = vertical.areas(area);
-
-        Paragraph::new("Connections:")
-            .bold()
-            .centered()
-            .render(header_area, buf);
+        let [conn_area, info_area] = vertical.areas(area);
 
         Paragraph::new("\nUse j/k to move. Enter to select connection")
             .centered()
-            .render(footer_area, buf);
+            .render(info_area, buf);
 
         let items = self
             .items
             .iter()
             .map(|conn| {
-                let line = Line::styled(conn.to_string(), colors::ALOE_GREEN);
-                ListItem::new(line).bg(colors::RICH_BLACK)
+                ListItem::new(Line::styled(
+                    conn.to_string(),
+                    colors::LIGHT_GOLDENROD_YELLOW,
+                ))
             })
             .collect::<Vec<_>>();
 
         let connections = List::new(items)
-            .block(Block::default().borders(Borders::ALL))
-            .highlight_style(Style::default().bg(colors::RICH_BLACK))
-            .style(Style::default());
+            .block(
+                Block::default()
+                    .title("Connections:")
+                    .borders(Borders::ALL)
+                    .fg(colors::ENERGY_YELLOW),
+            )
+            .highlight_symbol(">")
+            .highlight_style(Style::default().bg(colors::ALOE_GREEN));
 
-        StatefulWidget::render(connections, rest_area, buf, &mut self.state);
+        StatefulWidget::render(connections, conn_area, buf, &mut self.state);
+    }
+}
+
+impl OnEvent for ConnWidget {
+    fn on_event(&mut self, event: Event) -> EventOutcome {
+        let mut outcome = EventOutcome::Render;
+
+        let Event::Key(key) = event else {
+            return EventOutcome::Ignore;
+        };
+
+        if key.kind == KeyEventKind::Press {
+            use KeyCode::*;
+
+            match key.code {
+                Char('q') | Esc => outcome = EventOutcome::Exit,
+                Char('j') | Down => self.next_conn(),
+                Char('k') | Up => self.prev_conn(),
+                Char('l') | Right | Enter => todo!(),
+                _ => outcome = EventOutcome::Ignore,
+            }
+        }
+
+        outcome
     }
 }
--- dbuzzy/src/widgets/mod.rs
+++ dbuzzy/src/widgets/mod.rs
@@ -1,1 +1,3 @@
 mod connections;
+
+pub use connections::ConnWidget;
