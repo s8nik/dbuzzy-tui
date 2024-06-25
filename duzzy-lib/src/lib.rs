16805f4920aa82c43e6fdddd83a5e5ae03308d55
Nikita <nick.parser@gmail.com>
2024-06-25 23:07:04 +0200

    just one trait for widget

--- duzzy-lib/src/lib.rs
+++ duzzy-lib/src/lib.rs
@@ -2,28 +2,20 @@ pub mod colors;
 pub mod event;
 mod utils;
 
+use event::Input;
 use ratatui::{buffer::Buffer, layout::Rect};
 pub use utils::{ensure_config_dir, read_toml};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Component {
-    Editor,
-    Connections,
-}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum EventOutcome {
     Render,
     Ignore,
     Exit,
-    // Focus(Component),
-    // Forward,
+    Focus(&'static str),
 }
 
-pub trait OnInput {
-    fn on_input(&mut self, input: event::Input) -> EventOutcome;
-}
-
-pub trait Renderer {
-    fn render(self, area: Rect, buf: &mut Buffer);
+pub trait DuzzyWidget {
+    fn name() -> &'static str;
+    fn input(&mut self, input: Input) -> EventOutcome;
+    fn render(&mut self, area: Rect, buf: &mut Buffer);
 }
